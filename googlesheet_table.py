479c7ca4d2827085cb6ab14e5af981fa4a25ecca
MrShirokow <mr.shirokow@gmail.com>
2022-10-14 18:05:23 +0500

    add functionality for working with Google tables

---
+++ googlesheet_table.py
@@ -0,0 +1,39 @@
+import pygsheets
+import datetime
+
+from pygsheets.client import Client
+
+
+class GoogleTable:
+    def __init__(self, credence_service_file: str = "", googlesheet_file_url: str = ""):
+        self.credence_service_file: str = credence_service_file
+        self.googlesheet_file_url: str = googlesheet_file_url
+
+    def _get_googlesheet_by_url(self, googlesheet_client: Client) -> pygsheets.Spreadsheet:
+        sheets = googlesheet_client.open_by_url(self.googlesheet_file_url)
+        return sheets.sheet1
+
+    def _get_googlesheet_client(self) -> Client:
+        return pygsheets.authorize(service_file=self.credence_service_file)
+
+    def search_names(self, time_delta: int = 0, date_column: int = 3, name_column: int = 2) -> list | int:
+        names = []
+        search_date = (datetime.date.today() + datetime.timedelta(days=time_delta)).strftime('%d.%m')
+        print(search_date)
+        googlesheet_client: Client = self._get_googlesheet_client()
+        worksheet: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client)
+        try:
+            found_cells = worksheet.find(search_date, matchEntireCell=True, cols=(date_column, date_column))
+        except:
+            return -1
+        for cell in found_cells:
+            row = cell.row
+            date = worksheet.get_value((row, date_column))
+            name = worksheet.get_value((row, name_column))
+            names.append((date, name))
+        return names
+
+
+# google_table = GoogleTable('creds.json', 'https://docs.google.com/spreadsheets/d/1Ob4hFh8wIjBHmE2z_ZF7MZg_k0KzBXFdHPfVF_vVHSA')
+# result = google_table.search_names(10)
+# print(result)
